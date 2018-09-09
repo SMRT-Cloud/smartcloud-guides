324f2a325ee7b3e5eb91b326f6c028117d7483f5
telostia <telostia@gmail.com>
2018-09-10 04:53:52 +1000

    updated addnodes

--- guides/mn_smartc_auto.sh
+++ guides/mn_smartc_auto.sh
@@ -43,7 +43,8 @@ PASSW=`pwgen -1 20 -n`
 echo -e "${GREEN}Preparing config file ${NONE}";
 sudo mkdir $HOME/.smrtc
 
-printf "addnode=139.99.159.113\naddnode=139.99.197.135\n
+printf "addnode=104.236.114.249\naddnode=104.236.118.35\naddnode=104.131.118.192\naddnode=104.236.25.11\naddnode=188.166.209.120\naddnode=188.166.209.139\naddnode=167.99.65.33\naddnode=206.189.36.191\naddnode=167.99.222.129\naddnode=167.99.221.205\naddnode=139.99.202.198\naddnode=139.99.198.190\naddnode=206.189.91.171\naddnode=104.236.25.11\n
+addnode=139.99.159.113\naddnode=139.99.197.135\n
 addnode=139.99.202.60\n
 addnode=139.99.197.112\n
 addnode=139.99.196.73\n
