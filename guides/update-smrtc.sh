5e11105520d3c71b63656991f71ca2f460224278
telostia <telostia@gmail.com>
2018-12-11 22:42:22 +1100

    added update guide and script fix delay

--- guides/update-smrtc.sh
+++ guides/update-smrtc.sh
@@ -1,12 +1,13 @@
 #get file
 cd
-wget  https://github.com/SMRT-Cloud/smrtc/releases/download/v1.0.0.2/smrtc-linux.tar.gz
 smrtc-cli stop
+wget  https://github.com/SMRT-Cloud/smrtc/releases/download/v1.0.0.2/smrtc-linux.tar.gz
 sudo chmod +x 
 sudo tar -xvf smrtc-linux.tar.gz
 sudo rm smrtc-linux.tar.gz
 sudo rm /usr/local/bin/smrtc*
 sudo cp smrtc* /usr/local/bin
 sudo rm smrtc*
+sleep 3
 smrtcd
 watch smrtc-cli getinfo
